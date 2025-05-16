f3217f71d77e5dcaa22418979183ca14a87bb411
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-05-16 18:37:39 -0400

    add signals module with routing and components for counter, user info, and properties

---
+++ src/app/signals/pages/propertie-page/propertie-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './propertie-page.component.html',
+  styleUrl: './propertie-page.component.css'
+})
+export class PropertiePageComponent {
+
+}
