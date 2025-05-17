3597fff3788f7e6e72538147a75740b0d7385b71
Alvaro Richard Guarachi Ticona <alvaro.guarachi.ticona@gmail.com>
2025-05-17 02:39:03 -0400

    feat: enhance user info page with reactive user loading and display functionality

--- src/app/signals/services/user-service.service.ts
+++ src/app/signals/services/user-service.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { map, Observable, tap } from 'rxjs'
 import { SingleUserResponse, User } from '../interfaces/user-request.interface';
@@ -10,9 +10,16 @@ export class UserServiceService {
 
   private http = inject( HttpClient );
   private baseUrl = 'https://reqres.in/api/users';
+  private apiKey = 'reqres-free-v1';
 
   getUserById( id: number ): Observable<User>{
-    return this.http.get<SingleUserResponse>(`${ this.baseUrl }/${ id }`)
+
+    const headers = new HttpHeaders({
+      'x-api-key': this.apiKey
+    });
+
+
+    return this.http.get<SingleUserResponse>(`${ this.baseUrl }/${ id }`, { headers })
       .pipe(
         map( response => response.data ),
         tap( console.log ),
